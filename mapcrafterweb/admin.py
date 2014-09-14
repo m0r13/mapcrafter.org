794bebc0fa68a23ba110cb99046d0cf5b3534619
Moritz Hilscher <moritz.hilscher@gmail.com>
2014-09-14 23:21:05 +0200

    Back to the beginning... removed models again.

--- mapcrafterweb/admin.py
+++ mapcrafterweb/admin.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
 from django.contrib.admin.options import ModelAdmin
-from mapcrafterweb.models import Platform, Build
 
-class PlatformAdmin(ModelAdmin):
-    list_display = ["name", "download_dir"]
+#class BuildAdmin(ModelAdmin):
+#    list_display = ["date", "platform", "version", "filename"]
 
-class BuildAdmin(ModelAdmin):
-    list_display = ["date", "platform", "version", "filename"]
-
-admin.site.register(Platform, PlatformAdmin)
-admin.site.register(Build, BuildAdmin)
+#admin.site.register(Build, BuildAdmin)
--- mapcrafterweb/models.py
+++ mapcrafterweb/models.py
@@ -1,28 +1,3 @@
 from django.db.models.base import Model
 from django.db.models.fields import CharField, DateTimeField
-from django.db.models.fields.related import ForeignKey
 from django.utils import timezone
-
-class Platform(Model):
-    class Meta:
-        verbose_name = "platform"
-        verbose_name_plural = "platforms"
-    
-    name = CharField(max_length=255, verbose_name="name")
-    download_dir = CharField(max_length=255, default="", verbose_name="download directory")
-    
-    def __unicode__(self):
-        return self.name
-
-class Build(Model):
-    class Meta:
-        verbose_name = "build"
-        verbose_name_plural = "builds"
-    
-    date = DateTimeField(default=timezone.now, verbose_name="date")
-    platform = ForeignKey(Platform, verbose_name="platform")
-    version = CharField(max_length=255, verbose_name="version")
-    filename = CharField(max_length=255, verbose_name="filename")
-    
-    def __unicode__(self):
-        return "Build %s %s" % (self.version, self.platform)
