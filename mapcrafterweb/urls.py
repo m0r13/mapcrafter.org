6f35a96460303f648058b2375ed0c48c6cd132b5
Moritz Hilscher <moritz.hilscher@gmail.com>
2014-09-14 19:48:47 +0200

    Created django project/app

---
+++ mapcrafterweb/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, url
+import views
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'mapcrafterweb.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r"^(|index\.html)$", views.index, name="index"),
+)
