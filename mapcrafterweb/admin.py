f649ff51e2ede2778c5cdbaa2d43f60117a6a4c5
Moritz Hilscher <moritz.hilscher@gmail.com>
2014-09-14 20:28:36 +0200

    Started to work on database for Mapcrafter builds.

--- mapcrafterweb/admin.py
+++ mapcrafterweb/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.admin.options import ModelAdmin
+from mapcrafterweb.models import Platform, Build
 
-# Register your models here.
+class PlatformAdmin(ModelAdmin):
+    list_display = ["name"]
+
+class BuildAdmin(ModelAdmin):
+    list_display = ["platform", "version", "filename"]
+
+admin.site.register(Platform, PlatformAdmin)
+admin.site.register(Build, BuildAdmin)
--- mapcrafterweb/models.py
+++ mapcrafterweb/models.py
@@ -1,3 +1,27 @@
-from django.db import models
+from django.db.models.base import Model
+from django.db.models.fields import CharField
+from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
+
+class Platform(Model):
+    class Meta:
+        verbose_name = "platform"
+        verbose_name_plural = "platforms"
+    
+    name = CharField(max_length=255, verbose_name="name")
+    
+    def __unicode__(self):
+        return self.name
+
+class Build(Model):
+    class Meta:
+        verbose_name = "build"
+        verbose_name_plural = "builds"
+    
+    platform = ForeignKey(Platform, verbose_name="platform")
+    version = CharField(max_length=255, verbose_name="version")
+    filename = CharField(max_length=255, verbose_name="filename")
+    
+    def __unicode__(self):
+        return "Build %s %s" % (self.version, self.platform)
