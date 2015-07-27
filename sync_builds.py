231f7e06e0262f778ef052f599b82d492d24be31
Moritz Hilscher <moritz.hilscher@gmail.com>
2015-07-27 22:13:53 +0200

    Update existing packages, delete not existing packages.

--- sync_builds.py
+++ sync_builds.py
@@ -23,7 +23,7 @@ def timestamp_to_datetime(timestamp):
     utc_dt = datetime.datetime.utcfromtimestamp(timestamp).replace(tzinfo=pytz.utc)
     return local_tz.normalize(utc_dt.astimezone(local_tz))
 
-if __name__ == "__main__":    
+if __name__ == "__main__":
     if len(sys.argv) < 2:
         print "Usage: %s [dist_dir]" % sys.argv[0]
         sys.exit(1)
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     
     type_win, _ = PackageType.objects.get_or_create(name="win")
     type_deb, _ = PackageType.objects.get_or_create(name="deb")
-    packages = []
+    packages = {}
 
     for filename in os.listdir(os.path.join(dist_dir, "debian", "packages")):
         match = REGEX_DEB_PACKAGE.match(filename)
@@ -56,7 +56,8 @@ if __name__ == "__main__":
         package.url = "//mapcrafter.org/debian/packages/%s" % filename
         package.downloads = 0
         package.visible = False
-        packages.append(package)
+        type_arch_version = package.type, package.arch, package.version
+        packages[type_arch_version] = package
     
     for filename in os.listdir(os.path.join(dist_dir, "windows")):
         match = REGEX_WIN_PACKAGE.match(filename)
@@ -79,13 +80,24 @@ if __name__ == "__main__":
         package.url = "//mapcrafter.org/windows/%s" % filename
         package.downloads = 0
         package.visible = False
-        packages.append(package)
+        type_arch_version = package.type, package.arch, package.version
+        packages[type_arch_version] = package
     
-    for package in packages:
+    for type_arch_version, package in packages.items():
         try:
-            Package.objects.get(
+            p = Package.objects.get(
                 type=package.type, arch=package.arch,
                 version_major=package.version_major, version_minor=package.version_minor,
                 version_build=package.version_build, version_commit=package.version_commit)
+            p.date = package.date
+            p.url = package.url
+            p.save()
         except Package.DoesNotExist:
+            print "New package: %s" % package
             package.save()
+
+    for package in Package.objects.all():
+        type_arch_version = package.type, package.arch, package.version
+        if not type_arch_version in packages:
+            print "Removed package: %s" % package
+            package.delete()
